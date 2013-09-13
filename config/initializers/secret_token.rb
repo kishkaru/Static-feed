b62b7e19c55be91a7f279773449031b162fa483a
Kishan Karunaratne <kishan@karu.io>
2013-09-12 17:57:24 -0700

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticFeed::Application.config.secret_token = 'adc19cd3b0797be7cc44b2cfdfd30f96b8c3faf7c5df66251394b6f7acb1296b50542ee093e59bad5e28695e123b1f12f939a939ac12912b54a967dad2ae8560'
